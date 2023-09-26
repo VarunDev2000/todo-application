96f7f588368fce44e18de009fadd6b88344bc4a1
VarunDev2000 <varundev652000@gmail.com>
2023-09-26 23:58:01 +0530

    styles: add transition to app menu for mobile devices

--- src/components/AppMenu/AppMenu.vue
+++ src/components/AppMenu/AppMenu.vue
@@ -1,54 +1,56 @@
 <template>
-  <div id="appMenu" :class="[appMenu.isMenuOpen ? 'flex' : 'hidden']">
-    <div>
-      <div class="flex flex-row justify-between items-center">
-        <p class="title">Menu</p>
-        <font-awesome-icon
-          class="md:hidden text-2xl text-gray-700 hover:text-gray-900 cursor-pointer ml-3"
-          icon="fa-solid fa-xmark"
-          title="Close menu"
-          @click="toggleMenu(false)"
-        />
-      </div>
-
-      <hr />
-
+  <transition :name="isMediumSizeScreen ? '' : 'fade'">
+    <div id="appMenu" v-if="isMediumSizeScreen || appMenu?.isMenuOpen">
       <div>
-        <p class="subtitle">Tasks</p>
-        <div>
-          <app-menu-task-item
-            v-for="menuTask in MENU_TASKS"
-            :key="menuTask?.id"
-            :task="menuTask"
-            :selected="menuTask?.id === appMenu.selectedTask"
-            :count="getMenuTaskCount(menuTask?.id)"
+        <div class="flex flex-row justify-between items-center">
+          <p class="title">Menu</p>
+          <font-awesome-icon
+            class="md:hidden text-2xl text-gray-700 hover:text-gray-900 cursor-pointer ml-3"
+            icon="fa-solid fa-xmark"
+            title="Close menu"
+            @click="toggleMenu(false)"
           />
+        </div>
+
+        <hr />
+
+        <div>
+          <p class="subtitle">Tasks</p>
+          <div>
+            <app-menu-task-item
+              v-for="menuTask in MENU_TASKS"
+              :key="menuTask?.id"
+              :task="menuTask"
+              :selected="menuTask?.id === appMenu.selectedTask"
+              :count="getMenuTaskCount(menuTask?.id)"
+            />
+          </div>
+        </div>
+
+        <hr />
+
+        <div>
+          <p class="subtitle">Lists</p>
+          <div>
+            <app-menu-list-item
+              v-for="menuList in MENU_LISTS"
+              :key="menuList?.id"
+              :list="menuList"
+              :selected="menuList?.id === appMenu.selectedList"
+              :count="getMenuListCount(menuList?.id)"
+            />
+          </div>
         </div>
       </div>
 
-      <hr />
-
-      <div>
-        <p class="subtitle">Lists</p>
-        <div>
-          <app-menu-list-item
-            v-for="menuList in MENU_LISTS"
-            :key="menuList?.id"
-            :list="menuList"
-            :selected="menuList?.id === appMenu.selectedList"
-            :count="getMenuListCount(menuList?.id)"
-          />
-        </div>
+      <div
+        class="flex flex-row items-center px-3 py-2 mt-7 rounded-md cursor-pointer hover:bg-gray-200"
+      >
+        <font-awesome-icon icon="fa-solid fa-gear" />
+        <p class="pl-3">Settings</p>
       </div>
     </div>
-
-    <div
-      class="flex flex-row items-center px-3 py-2 mt-7 rounded-md cursor-pointer hover:bg-gray-200"
-    >
-      <font-awesome-icon icon="fa-solid fa-gear" />
-      <p class="pl-3">Settings</p>
-    </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -66,15 +68,26 @@ export default {
   },
   data() {
     return {
+      isMediumSizeScreen: false,
       MENU_TASKS,
       MENU_LISTS
     }
+  },
+  mounted() {
+    window.addEventListener('resize', this.checkIsMediumSizeScreen)
+    this.checkIsMediumSizeScreen()
+  },
+  beforeUnmount() {
+    window.removeEventListener('resize', this.checkIsMediumSizeScreen)
   },
   computed: {
     ...mapState(['appMenu', 'todo'])
   },
   methods: {
     ...mapActions(['toggleMenu']),
+    checkIsMediumSizeScreen() {
+      this.isMediumSizeScreen = window.innerWidth >= 768
+    },
     getFilteredData,
     getMenuTaskCount(menuTask) {
       return this.getFilteredData(this.todo, menuTask).length
@@ -88,7 +101,7 @@ export default {
 
 <style lang="postcss" scoped>
 #appMenu {
-  @apply w-screen h-screen  md:flex flex-col justify-between rounded-xl px-5 py-4 md:w-[340px] md:h-full;
+  @apply w-screen h-screen fixed flex-col justify-between rounded-xl px-5 py-4 md:static md:w-[340px] md:h-full;
   background-color: #f4f4f4;
   overflow-x: hidden;
   overflow-y: auto;
