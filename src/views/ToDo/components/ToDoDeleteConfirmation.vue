b1f81c53182c4178741fad392a001410c582b6d7
VarunDev2000 <varundev652000@gmail.com>
2023-09-29 18:08:05 +0530

    refactor: split to be deleted data component from delete confirmation modal component

--- src/views/ToDo/components/ToDoDeleteConfirmation.vue
+++ src/views/ToDo/components/ToDoDeleteConfirmation.vue
@@ -15,11 +15,13 @@
       </p>
       <hr />
 
-      <div v-if="!Array.isArray(data)"><ToBeDeletedData :todo="data" /></div>
+      <div v-if="!Array.isArray(data)">
+        <todo-delete-confirmation-data :todo="data" />
+      </div>
 
       <div v-else>
         <div v-for="todoId in data" :key="todoId">
-          <tobe-deleted-data :todo="getToDoById(todoId)" />
+          <todo-delete-confirmation-data :todo="getToDoById(todoId)" />
         </div>
       </div>
     </div>
@@ -29,45 +31,15 @@
 </template>
 
 <script>
-import Vue from 'vue'
 import { mapState } from 'vuex'
 import AppModal from '@/components/AppModal'
-
-const ToBeDeletedData = Vue.component('ToBeDeletedData', {
-  props: {
-    todo: {
-      type: Object,
-      default: null
-    }
-  },
-  template: `<div>
-    <p class="w-full mx-3 py-3">
-          {{ todo.task }}
-        </p>
-
-        <div class="pl-7 pr-4 pb-3 text-[13px]">
-          <p class="italic">{{ todo.description }}</p>
-
-          <div class="flex flex-row mt-2">
-            <div class="flex flex-row items-center">
-              <font-awesome-icon
-                style="margin-bottom: 2px"
-                icon="fa-solid fa-calendar-xmark"
-                title="Due date"
-              />
-              <p class="ml-2">{{ todo.date }}</p>
-            </div>
-          </div>
-        </div>
-        <hr />
-    </div>`
-})
+import ToDoDeleteConfirmationData from './ToDoDeleteConfirmationData'
 
 export default {
   name: 'ToDoDeleteConfirmation',
   components: {
     modal: AppModal,
-    'tobe-deleted-data': ToBeDeletedData
+    'todo-delete-confirmation-data': ToDoDeleteConfirmationData
   },
   props: {
     data: {
---
+++ src/views/ToDo/components/ToDoDeleteConfirmationData.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <p
+      class="w-[97%] py-3 mx-3 break-words"
+      :class="{
+        'break-all': hasNoSpaces(todo.task)
+      }"
+    >
+      {{ todo.task }}
+    </p>
+
+    <div class="text-[13px] pl-7 pr-4 pb-3">
+      <p
+        class="italic"
+        :class="[
+          todo.description !== '' ? 'mb-4' : '',
+          hasNoSpaces(todo.task) ? 'break-all' : 'break-words'
+        ]"
+      >
+        {{ todo.description }}
+      </p>
+
+      <div class="flex flex-row mt-0">
+        <div class="flex flex-row items-center">
+          <font-awesome-icon
+            style="margin-bottom: 2px"
+            icon="fa-solid fa-calendar-xmark"
+            title="Due date"
+          />
+          <p class="ml-2">{{ todo.date }}</p>
+        </div>
+      </div>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+import { hasNoSpaces } from '@/utils/helpers'
+
+export default {
+  name: 'ToDoDeleteConfirmationData',
+  props: {
+    todo: {
+      type: Object,
+      default: null
+    }
+  },
+  methods: {
+    hasNoSpaces
+  }
+}
+</script>
+
+<style scoped></style>
